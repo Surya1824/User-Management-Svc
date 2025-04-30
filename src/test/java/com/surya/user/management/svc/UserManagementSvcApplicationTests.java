c1df880fe87c1f33591c91a2efe45b798ee631c6
Surya1824 <suryateja5347@gmail.com>
2025-04-30 20:36:27 +0530

    first commit

---
+++ src/test/java/com/surya/user/management/svc/UserManagementSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surya.user.management.svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
