c957a9d1122b8cb8a667436a31f7d9fbf1b7abc5
Surya1824 <suryateja5347@gmail.com>
2025-05-03 16:58:02 +0530

    Configured Discovery Client

--- src/main/java/com/surya/user/management/svc/UserManagementSvcApplication.java
+++ src/main/java/com/surya/user/management/svc/UserManagementSvcApplication.java
@@ -2,8 +2,10 @@ package com.surya.user.management.svc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserManagementSvcApplication {
 
 	public static void main(String[] args) {
